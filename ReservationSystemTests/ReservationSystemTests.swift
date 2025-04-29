047c433e7df943cbbc182abf07a84cfc995a863c
Guilherme Higa <guilhermehiga11@gmail.com>
2025-04-29 14:51:56 -0700

    Initial Commit

---
+++ ReservationSystemTests/ReservationSystemTests.swift
@@ -0,0 +1,17 @@
+//
+//  ReservationSystemTests.swift
+//  ReservationSystemTests
+//
+//  Created by Guilherme Higa on 4/29/25.
+//
+
+import Testing
+@testable import ReservationSystem
+
+struct ReservationSystemTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
